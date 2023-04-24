f9221f6b594946b4efcd89ea918b8ffdb298007e
soroosh <sorosh18najafi@gmail.com>
2023-04-24 14:23:57 +0200

    Adding some changes

--- grafiek1.php
+++ grafiek1.php
@@ -9,37 +9,37 @@
 </head>
 <body>
     <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "energy_db";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "energy_db";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT * FROM energy_users";
-    $result = $conn->query($sql);
-
-    $data = array();
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $data[] = $row;
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-    } else {
-        echo "0 results";
-    }
 
-    $conn->close();
+        $sql = "SELECT * FROM energy_users";
+        $result = $conn->query($sql);
+
+        $data = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        } else {
+            echo "0 results";
+        }
+
+        $conn->close();
     ?>
 
-    <canvas id="energyConsumptionChart" width="400" height="200"></canvas>
-    <canvas id="contractTypeChart" width="400" height="200"></canvas>
-    <canvas id="energyPriceChart" width="400" height="200"></canvas>
-    <canvas id="contractDateChart" width="400" height="200"></canvas>
+    <canvas id="energyConsumptionChart" width="100" height="50"></canvas>
+    <canvas id="contractTypeChart" width="100" height="50"></canvas>
+    <canvas id="energyPriceChart" width="100" height="50"></canvas>
+    <canvas id="contractDateChart" width="100" height="50"></canvas>
 
     <script>
         function createChart(type, canvasId, chartLabel, chartData, backgroundColor, borderColor) {
@@ -70,9 +70,42 @@
         const labels = userData.map(user => user.name);
 
         createChart('bar', 'energyConsumptionChart', 'Energieverbruik (kWh)', { labels: labels, values: userData.map(user => user.energy_consumption) }, 'rgba(75, 192, 192, 0.2)', 'rgba(75, 192, 192, 1)');
-        createChart('bar', 'contractTypeChart', 'Contract Type', { labels: labels, values: userData.map(user => user.contract_type) }, 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 1)');
+
+        function countOccurrences(arr) {
+            return arr.reduce((acc, curr) => {
+                acc[curr] = (acc[curr] || 0) + 1;
+                return acc;
+            }, {});
+        }
+
+        const contractTypeData = userData.map(user => user.contract_type);
+        const occurrences = countOccurrences(contractTypeData);
+        const uniqueContractTypes = Array.from(new Set(contractTypeData));
+        const contractTypeValues = uniqueContractTypes.map(type => occurrences[type]);
+
+        createChart('pie', 'contractTypeChart', '', {
+            labels: uniqueContractTypes,
+            values: contractTypeValues
+        }, ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(255, 206, 86, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)', 'rgba(255, 159, 64, 0.5)']);
+
         createChart('bar', 'energyPriceChart', 'Energieprijs (€)', { labels: labels, values: userData.map(user => user.energy_price) }, 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 1)');
-        createChart('line', 'contractDateChart', 'Contractdatum', { labels: labels, values: userData.map(user => new Date(user.contract_date).getDate()) }, 'rgba(153, 102, 255, 0.2)', 'rgba(153, 102, 255, 1)');
+
+        function countOccurrencesPerYear(arr) {
+            return arr.reduce((acc, curr) => {
+                acc[curr] = (acc[curr] || 0) + 1;
+                return acc;
+            }, {});
+        }
+
+        const yearData = userData.map(user => new Date(user.contract_date).getFullYear());
+        const occurrencesPerYear = countOccurrencesPerYear(yearData);
+        const years = Array.from({ length: 2026 - 2010 }, (_, i) => i + 2010);
+        const yearValues = years.map(year => occurrencesPerYear[year] || 0);
+
+        createChart('line', 'contractDateChart', 'Contractdatum', {
+            labels: years,
+            values: yearValues
+        }, 'rgba(153, 102, 255, 0.2)', 'rgba(153, 102, 255, 1)');
     </script>
 </body>
 </html>
